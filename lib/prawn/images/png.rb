6860aad5926655b1f715ed6a2b3341c7a860f181
James Healy <jimmy@deefa.com>
2011-05-05 15:07:05 +1000

    optimise common cases in PNG class
    * only color types 4 and 6 need to be fully unfiltered before embedded
    * color types 0, 2 and 3 can be embedded without performing the
      expensive un-filtering step, they just need a few options set correctly
      on the XObject

--- lib/prawn/images/png.rb
+++ lib/prawn/images/png.rb
@@ -19,7 +19,7 @@ module Prawn
     #
     class PNG < Image
       attr_reader :img_data, :alpha_channel
-      attr_reader :width, :height
+      attr_reader :width, :height, :color_type
       attr_accessor :scaled_width, :scaled_height
 
       def self.can_render?(image_blob)
@@ -32,14 +32,29 @@ module Prawn
       #
       def initialize(data)
         @chunky = ChunkyPNG::Image.from_blob(data)
+        @ds     = ChunkyPNG::Datastream.from_blob(data)
         @width  = @chunky.width
         @height = @chunky.height
-        @img_data = extract_image_data
-        @alpha_channel = extract_alpha_channel
+        @color_type = extract_color_type
+
+        case @color_type
+        when 0,2,3
+          @img_data      = filtered_image_data
+          @alpha_channel = nil
+        when 4,6
+          @img_data      = unfiltered_image_data
+          @alpha_channel = unfiltered_alpha_channel
+        else
+          raise Errors::UnsupportedImageType, "Unsupported PNG color type #{@color_type}"
+        end
       end
 
       def alpha_channel?
         @alpha_channel != nil
+      end
+
+      def no_alpha_channel?
+        @alpha_channel.nil?
       end
 
       # Build a PDF object representing this image in +document+, and return
@@ -55,13 +70,13 @@ module Prawn
           :BitsPerComponent => bits,
           :Length           => @img_data.size,
           :Filter           => :FlateDecode,
-          :ColorSpace       => colorspace
+          :ColorSpace       => colorspace(document)
         )
 
         # append the actual image data to the object as a stream
         obj << @img_data
 
-        if @alpha_channel
+        if alpha_channel?
           smask_obj = document.ref!(
             :Type             => :XObject,
             :Subtype          => :Image,
@@ -75,6 +90,11 @@ module Prawn
           )
           smask_obj << @alpha_channel
           obj.data[:SMask] = smask_obj
+        else
+          obj.data[:DecodeParms] = {:Predictor => 15,
+                                    :Colors    => self.colors,
+                                    :BitsPerComponent => self.bits,
+                                    :Columns   => self.width}
         end
 
         obj
@@ -92,17 +112,45 @@ module Prawn
         8
       end
 
-      def colorspace
-        if @chunky.palette.grayscale?
-          :DeviceGray
-        else
-          :DeviceRGB
+      # number of color components to each pixel
+      #
+      def colors
+        case self.color_type
+        when 0, 3, 4
+          return 1
+        when 2, 6
+          return 3
         end
+      end
+
+      def colorspace(document)
+        @colorspace ||= if @ds.palette_chunk
+                          chunk = @ds.palette_chunk
+                          palette_obj = document.ref!(:Length => chunk.content.size)
+                          palette_obj << chunk.content
+                          [:Indexed, :DeviceRGB, (chunk.content.size / 3) -1, palette_obj]
+                        elsif colors == 1
+                          :DeviceGray
+                        else
+                          :DeviceRGB
+                        end
       end
 
       private
 
-      def extract_image_data
+      # return the PNG color type of this image
+      #
+      def extract_color_type
+        @ds.header_chunk.color
+      end
+
+      # unfilters the compressed PNG data and extracts raw pixel data WITHOUT
+      # the alpha channel.
+      #
+      # This is expensive, only do it on PNG types 4 and 6 where we have no
+      # option but to split out the alpha channel.
+      #
+      def unfiltered_image_data
         if @chunky.palette.grayscale?
           raw = @chunky.to_grayscale_stream
         else
@@ -111,7 +159,13 @@ module Prawn
         Zlib::Deflate.deflate(raw)
       end
 
-      def extract_alpha_channel
+      # unfilters the compressed PNG data and extracts raw alpha channel WITHOUT
+      # the color data. There will always be 1 byte per pixel.
+      #
+      # This is expensive, only do it on PNG types 4 and 6 where we have no
+      # option but to split out the alpha channel.
+      #
+      def unfiltered_alpha_channel
         raw = @chunky.to_alpha_channel_stream
 
         if raw.match(/\A\xFF+\Z/)
@@ -119,6 +173,17 @@ module Prawn
         else
           Zlib::Deflate.deflate(raw)
         end
+      end
+
+      # return the compressed pixel data from the unerlying image. The data is
+      # pre-processed with the PNG predictors then compressed with zlib.
+      #
+      # If the image has no alpha channel (color type 1, 2 and 3) then there is
+      # no need to perform the expensive unfiltering operations. It can be
+      # embedded as is.
+      #
+      def filtered_image_data
+        @ds.data_chunks.map { |c| c.content }.join('')
       end
     end
   end
