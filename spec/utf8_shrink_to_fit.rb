ebeea758f2ce1e5d3fd240a7af3cc4e6dcbc6716
Christian Szardenings <chris@szar.de>
2013-12-19 00:02:22 +0100

    Working test case
    Specifying DejaVu fonts explicetely does prevent this bug.

--- spec/utf8_shrink_to_fit.rb
+++ spec/utf8_shrink_to_fit.rb
@@ -3,14 +3,17 @@ require File.join(File.expand_path(File.dirname(__FILE__)), "spec_helper")
 
 describe "#shrink_to_fit with special utf-8 text" do
   it "Should not throw an exception" do
-    pages = 0
     doc = Prawn::Document.new(page_size: 'A4', margin: [2, 2, 2, 2]) do |pdf|
       add_unicode_fonts(pdf)
-      pdf.bounding_box([1, 1], :width => 90, :height => 50) do
+      pdf.bounding_box([90, 800], :width => 90, :height => 50) do
+        pdf.stroke_bounds
         broken_text = " Sample Text\nSAMPLE SAMPLE SAMPLEoddělení ZMĚN\nSAMPLE"
+        pdf.font 'dejavu'
         pdf.text broken_text, :overflow => :shrink_to_fit
       end
     end
+    file = doc.render
+    File.write("/tmp/test.pdf", file)
   end
 end
 
